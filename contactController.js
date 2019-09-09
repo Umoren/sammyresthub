bf83a8386dd68e0af5bd7febed116330ce26cdae
Umoren <samuelumoren365@gmail.com>
2019-09-09 11:37:33 +0100

    api is working

--- contactController.js
+++ contactController.js
@@ -25,9 +25,8 @@ exports.new = function (req, res) {
     contact.phone = req.body.phone;
 // save the contact and check for errors
     contact.save(function (err) {
-        // if (err)
-        //     res.json(err);
-res.json({
+       
+        res.json({
             message: 'New contact created!',
             data: contact
         });
@@ -49,7 +48,7 @@ exports.update = function (req, res) {
 Contact.findById(req.params.contact_id, function (err, contact) {
         if (err)
             res.send(err);
-contact.name = req.body.name ? req.body.name : contact.name;
+        contact.name = req.body.name ? req.body.name : contact.name;
         contact.gender = req.body.gender;
         contact.email = req.body.email;
         contact.phone = req.body.phone;
@@ -71,9 +70,9 @@ exports.delete = function (req, res) {
     }, function (err, contact) {
         if (err)
             res.send(err);
-res.json({
-            status: "success",
-            message: 'Contact deleted'
+            res.json({
+                status: "success",
+                message: 'Contact deleted'
         });
     });
 };
